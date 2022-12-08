b64faf0ac3bdde2e5f9db37ba1d0caa53d0c6e69
CsChristian20 <119200892+CsChristian20@users.noreply.github.com>
2022-12-07 21:11:02 -0400

    ejercicios-uno

---
+++ hack_4/hack_4_map.js
@@ -0,0 +1,15 @@
+/**
+ * mediante el map for agregar los números 1,3,5,7
+ * dentro del array result 
+ * 
+ * 
+ * output => [1,3,5,7]
+ */
+let result = [1,3,5,7];
+
+result.map(function(items){
+    return items
+})
+
+//export result
+module.exports = result;
