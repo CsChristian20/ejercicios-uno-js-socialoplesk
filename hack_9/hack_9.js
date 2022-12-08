7805b76be221759a3d54a544c5f3b7a8648481f4
CsChristian20 <119200892+CsChristian20@users.noreply.github.com>
2022-12-07 21:12:52 -0400

    ejercicios-uno

---
+++ hack_9/hack_9.js
@@ -0,0 +1,26 @@
+/**
+ * ["foo","bar","baz","qux","echo"] 
+ * 
+ * contar el total de items que inicien con el caracter "b"
+ *  
+ * 
+ * output => 2   
+ */
+let arr = ["foo","bar","baz","qux","echo"];
+let result = 0;
+
+let i = 0
+let len = arr.length;
+
+for(i; i < len; i++){
+
+    if (arr[i][0] == 'b' ){
+        result ++;
+    } 
+    
+}
+
+
+
+//export result
+module.exports = result;
